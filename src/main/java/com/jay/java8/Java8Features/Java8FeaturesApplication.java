dc1d760066aa1b4febbb5b699b3fc10bbc9fa9e3
Jayhind Rajpoot <jayhindraj@outlook.com>
2018-05-09 16:59:45 +0530

    Initial Commit
    Draft

---
+++ src/main/java/com/jay/java8/Java8Features/Java8FeaturesApplication.java
@@ -0,0 +1,12 @@
+package com.jay.java8.Java8Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8FeaturesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jay/java8/Java8Features/Java8FeaturesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jay.java8.Java8Features;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java8FeaturesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
